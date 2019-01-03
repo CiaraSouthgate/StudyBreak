2c50744b0419daf6b01b85f6872b0333d158ce4a
Ciara Southgate <ciarajsouthgate@gmail.com>
2019-01-02 22:54:37 -0800

    DisplayTimer goes to InterruptionTimer Activity when time's up

--- app/src/main/java/com/ciarasouthgate/studybreak/DisplayInterruption.java
+++ app/src/main/java/com/ciarasouthgate/studybreak/DisplayInterruption.java
@@ -1,13 +1,28 @@
 package com.ciarasouthgate.studybreak;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class DisplayInterruption extends AppCompatActivity {
+    private static final int MILLI_IN_MINUTE = 60000;
+    private static final int MILLI_IN_SECOND = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_interruption);
+    }
+
+    public static void countdownInterruption(long runningTime, Interruption task) {
+        new CountDownTimer(runningTime, MILLI_IN_SECOND) {
+            public void onTick(long millisUntilFinished) {
+                //TODO update displayed timer
+            }
+
+            public void onFinish() {
+                //TODO make notification
+            }
+        }.start();
     }
 }
