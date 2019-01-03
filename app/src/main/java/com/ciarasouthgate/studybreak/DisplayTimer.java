ba68ee1cb52a17df998eb21a5433d01720dbc590
YummyBlaBla <gorick2491@hotmail.com>
2019-01-03 10:06:56 -0800

    Integrated TimerService

--- app/src/main/java/com/ciarasouthgate/studybreak/DisplayTimer.java
+++ app/src/main/java/com/ciarasouthgate/studybreak/DisplayTimer.java
@@ -1,13 +1,17 @@
 package com.ciarasouthgate.studybreak;
 
 import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.os.CountDownTimer;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -23,12 +27,25 @@ public class DisplayTimer extends AppCompatActivity {
     private static final int SIX_HOURS = 6 * 60 * 60000;
     private long startTime;
 
+
+
     TextView waterTime;
     TextView stretchTime;
     TextView foodTime;
     TextView otherTime;
 
     StudySession session;
+
+    private BroadcastReceiver br = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            String keyID = intent.getStringExtra("name");
+            String timeString = intent.getStringExtra("time");
+
+            updateText(keyID, timeString);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,23 +57,71 @@ public class DisplayTimer extends AppCompatActivity {
         foodTime = findViewById(R.id.foodTime);
         otherTime = findViewById(R.id.otherTime);
 
-//        waterTime.setText("test");
-//        stretchTime.setText("test");
-//        foodTime.setText("test");
-//        otherTime.setText("test");
-
         session = getIntent().getParcelableExtra("session");
         startTime = getIntent().getLongExtra("startTime", SIX_HOURS);
         notificationManager = NotificationManagerCompat.from(this);
 
-        countdownStudy(SIX_HOURS, session.getInterruptions());
-
         /* These next few lines are for the Service, if we get it running. */
         Intent serviceIntent = new Intent(this, TimerService.class);
-        serviceIntent.putExtra("session", session);
-
-        this.startService(serviceIntent);
+        serviceIntent.putExtra("session", this.session);
+        startService(serviceIntent);
         /* End of Service code */
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        registerReceiver(br, new IntentFilter(TimerService.DISPLAY_TIMER_BR));
+        System.out.println("resume");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        unregisterReceiver(br);
+    }
+
+    @Override
+    public void onStop() {
+        try {
+            unregisterReceiver(br);
+        } catch (Exception e) {
+            System.out.println("Already unregistered");
+        }
+
+        super.onStop();
+    }
+
+    public void updateText(String taskname, String timeRemaining) {
+        switch (taskname) {
+            case ("water"):
+                waterTime.setText(timeRemaining);
+                break;
+            case ("stretch"):
+                stretchTime.setText(timeRemaining);
+                break;
+            case("food"):
+                foodTime.setText(timeRemaining);
+                break;
+            case("other"):
+                otherTime.setText(timeRemaining);
+                break;
+            default:
+                break;
+        }
+        System.out.println(timeRemaining);
+
+        long timeLong = Long.parseLong(timeRemaining.trim());
+        if (timeLong * MILLI_IN_MINUTE <= 10000) {
+
+            Interruption[] interruptions = session.getInterruptions();
+
+            for (Interruption interruption : interruptions) {
+                if (interruption.getName().equals(taskname)) {
+                    startInterruption(interruption, timeLong);
+                }
+            }
+        }
     }
 
     public void countdownStudy(long runningTime, final Interruption[] tasks) {
--- app/src/main/java/com/ciarasouthgate/studybreak/TimerService.java
+++ app/src/main/java/com/ciarasouthgate/studybreak/TimerService.java
@@ -8,22 +8,54 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
 import android.os.CountDownTimer;
+import android.util.Log;
 
 import static com.ciarasouthgate.studybreak.App.CHANNEL_2_ID;
 
 public class TimerService extends Service {
     private static final int SIX_HOURS = 6 * 60 * 60000;
+    private static final int MILLI_IN_MINUTE = 60000;
     CountDownTimer countdowntimer;
     private StudySession session;
+    public static final String DISPLAY_TIMER_BR = "com.ciarasouthgate.studybreak.DisplayTimer";
+    Intent bi = new Intent(DISPLAY_TIMER_BR);
 
     @Override
     public void onCreate() {
         super.onCreate();
-
+        Log.i("TimerService","Start Timer Service");
         countdowntimer = new CountDownTimer(SIX_HOURS, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
+                Interruption[] tasks = session.getInterruptions();
+                for (Interruption task : tasks) {
+                    long remainingTime = millisUntilFinished % task.getInterval();
+                    String timeString;
+                    timeString = Long.toString(remainingTime / MILLI_IN_MINUTE) + " ";
 
+                    switch (task.getName()) {
+                        case ("water"):
+                            bi.putExtra("time", timeString);
+                            bi.putExtra("name", "water");
+                            break;
+                        case ("stretch"):
+                            bi.putExtra("time", timeString);
+                            bi.putExtra("name", "stretch");
+                            break;
+                        case("food"):
+                            bi.putExtra("time", timeString);
+                            bi.putExtra("name", "food");
+                            break;
+                        case("other"):
+                            bi.putExtra("other", timeString);
+                            bi.putExtra("name", "other");
+                            break;
+                        default:
+                            break;
+                    }
+                    sendBroadcast(bi);
+                    System.out.println("send");
+                }
             }
             @Override
             public void onFinish() {
@@ -40,9 +72,7 @@ public class TimerService extends Service {
         Bundle extras = intent.getExtras();
 
         if (!(extras == null)) {
-
-            session = intent.getParcelableExtra("session");
-
+            this.session = intent.getParcelableExtra("session");
         }
         return super.onStartCommand(intent, flags, startId);
     }
